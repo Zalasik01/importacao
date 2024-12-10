9fe3fa4a9fd3ef171a6a89add8f2943ccc493572
Nicolas <nicolas.souza@altimus.com.br>
2024-12-10 14:31:52 -0300

    Ajustes no output

--- dist/RevendaMais/Estoque_Veiculos/estoque_veiculos.py
+++ dist/RevendaMais/Estoque_Veiculos/estoque_veiculos.py
@@ -38,8 +38,14 @@ def processar_planilhas(input_file_1, input_file_2=None, output_file=None):
             "VALOR COMPRA": "VALOR COMPRA",
             "VALOR VENDA": "VALOR DE VENDA",
             "KM": "KM",
+            "FORNECEDOR": "NOME PROPRIETARIO ENTRADA",
             "CPF/CNPJ FORNECEDOR": "CPF/CNPJ PROPRIETARIO ENTRADA"
         }
+        # Alterar a coluna POSICAO ESTOQUE para 0 em todos os registros da primeira planilha
+        if "POSICAO ESTOQUE" in df1.columns:
+            df1["POSICAO ESTOQUE"] = 0
+        else:
+            print("A coluna 'POSICAO ESTOQUE' não foi encontrada no DataFrame.")
 
         # Mapear colunas da planilha 2
         mapping_2 = {
@@ -55,7 +61,7 @@ def processar_planilhas(input_file_1, input_file_2=None, output_file=None):
             "TIPO": "TIPO",
             "VALOR COMPRA": "VALOR COMPRA",
             "VALOR VENDA": "VALOR DE VENDA",
-            "VENDEDOR": "NOME PROPRIETARIO ENTRADA",
+            "CLIENTE": "NOME PROPRIETARIO ENTRADA",
             "CPF/CNPJ CLIENTE": "CPF/CNPJ PROPRIETARIO ENTRADA",
             "KM": "KM",
             "POSICAO ESTOQUE": "STATUS"
@@ -93,20 +99,60 @@ def processar_planilhas(input_file_1, input_file_2=None, output_file=None):
         df_final = pd.DataFrame(columns=colunas_final)
 
         # Mapear os dados ao DataFrame final
+        df_final["ID"] = df_combinado.get("ID", None)
         df_final["STATUS"] = df_combinado.get("STATUS", None)
         df_final["DATA DE ENTRADA"] = df_combinado.get("DATA DE ENTRADA", None)
         df_final["DATA E HORA DE SAIDA"] = df_combinado.get("DATA E HORA DE SAIDA", None)
         df_final["MARCA"] = df_combinado.get("MARCA", None)
         df_final["MODELO"] = df_combinado.get("MODELO", None)
+        df_final["COMPLEMENTO"] = df_combinado.get("COMPLEMENTO", None)
         df_final["CHASSI"] = df_combinado.get("CHASSI", None)
         df_final["RENAVAM"] = df_combinado.get("RENAVAM", None)
+        df_final["NUMERO MOTOR"] = df_combinado.get("NUMERO MOTOR", None)
         df_final["ANO MOD"] = df_combinado.get("ANO MOD", None)
         df_final["COR"] = df_combinado.get("COR", None)
         df_final["COMBUSTIVEL"] = df_combinado.get("COMBUSTIVEL", None)
         df_final["PLACA"] = df_combinado.get("PLACA", None)
         df_final["TIPO"] = df_combinado.get("TIPO", None)
+        df_final["VALOR COMPRA"] = df_combinado.get("VALOR COMPRA", None)
+        df_final["VALOR A VISTA"] = df_combinado.get("VALOR A VISTA", None)
+        df_final["VALOR DE VENDA"] = df_combinado.get("VALOR DE VENDA", None)
+        df_final["NOME PROPRIETARIO ENTRADA"] = df_combinado.get("NOME PROPRIETARIO ENTRADA", None)
+        df_final["CPF/CNPJ PROPRIETARIO ENTRADA"] = df_combinado.get("CPF/CNPJ PROPRIETARIO ENTRADA", None)
+        df_final["KM"] = df_combinado.get("KM", None)
+        df_final["PORTAS"] = df_combinado.get("PORTAS", None)
+        df_final["CAMBIO"] = df_combinado.get("CAMBIO", None)
+        df_final["CNPJ REVENDA"] = df_combinado.get("CNPJ REVENDA", None)
         df_final["ESTADO_CONVERSACAO"] = "USADO"
 
+        # Tratamento da coluna COMBUSTIVEL
+        if "COMBUSTIVEL" in df_combinado.columns:
+            df_final["COMBUSTIVEL"] = df_combinado["COMBUSTIVEL"].replace({
+                "FLEX": "ALCOOL/GASOLINA"
+            })
+        else:
+            print("A coluna 'COMBUSTIVEL' não foi encontrada no DataFrame.")
+
+        # Tratamento da coluna TIPO
+        if "TIPO" in df_combinado.columns:
+            df_final["TIPO"] = df_combinado["TIPO"].replace({
+                "Consignado": "TERCEIRO_CONSIGNADO",
+                "Proprio": "PROPRIO"
+            })
+        else:
+            print("A coluna 'TIPO' não foi encontrada no DataFrame.")
+
+        # Ajustar VALOR A VISTA para ser igual ao VALOR DE VENDA caso esteja null
+        df_final["VALOR A VISTA"] = df_final.apply(
+            lambda row: row["VALOR DE VENDA"] if pd.isnull(row["VALOR A VISTA"]) else row["VALOR A VISTA"],
+            axis=1
+        )
+        # Tratar valores null na coluna "ESTADO_CONVERSACAO"
+        df_final["ESTADO_CONVERSACAO"] = df_final["ESTADO_CONVERSACAO"].fillna("USADO")
+        # Tratar valores da coluna "ANO FAB." para serem iguais aos valores de "ANO MOD"
+        df_final["ANO FAB."] = df_final["ANO MOD"]
+        # Dividir a coluna MODELO em MODELO e COMPLEMENTO
+        df_final[["MODELO", "COMPLEMENTO"]] = df_final["MODELO"].str.split(n=1, expand=True)
         # Configurar formatação para saída no Excel
         with pd.ExcelWriter(output_file, engine='xlsxwriter') as writer:
             df_final.to_excel(writer, index=False, sheet_name='Planilha Final')
